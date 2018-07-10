1be45f5475937786a2e2152d4f400db85be7d1e5
Natilu <38574035+natilu01@users.noreply.github.com>
2018-07-10 09:00:42 -0500

    Add files via upload

---
+++ PedidoFrutas/src/pedidofrutas/Clientes.java
@@ -0,0 +1,26 @@
+
+package pedidofrutas;
+
+public class Clientes {
+    private String nombre;
+    private int cedula;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+   
+    
+}
---
+++ PedidoFrutas/src/pedidofrutas/Frutas.java
@@ -0,0 +1,47 @@
+
+package pedidofrutas;
+
+public class Frutas {
+    private String codigo;
+    private String nombre;
+    private int precio;
+    private int frutas;
+
+    Frutas(String text, String text0, String text1, String Frutas) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int getFrutas() {
+        return frutas;
+    }
+
+    public void setFrutas(int frutas) {
+        this.frutas = frutas;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    
+}
